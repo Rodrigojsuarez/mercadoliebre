e2355152a391c197250a8683d2a5743df02bb61a
Rodrigojsuarez <rodrysuarez96@gmail.com>
2024-02-27 22:36:07 -0300

    segundo commit

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require("express");
 const app = express();
 const path = require("path");
 app.use(express.static("public"));
-app.listen(3000 , () => 
-console.log("servidor online"));
+const port = process.env.PORT || 3000;
+app.listen(port, () => 
+console.log(`servidor corriendo en el puerto ${port}`));
 
 app.get("/", (req, res) => {
     res.sendFile(path.join(__dirname, "/views/home.html"))
